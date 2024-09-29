f3d9391e08d9e09655cabfaa0994eb90c46a9d26
Nathan Daniel <20nmd1@queensu.ca>
2024-09-29 17:17:45 -0400

    completed task 2

--- Assign1_ELEC376_starter_code/a1_task2/hamletStart.cpp
+++ Assign1_ELEC376_starter_code/a1_task2/hamletStart.cpp
@@ -1,120 +1,139 @@
-/* The program allows users to find and report the frequency 
- * of a specific word within the text of "Hamlet." 
- * The program prompts the user to enter a word, 
- * reads the text from the "hamlet.txt" file, and searches
- * for occurrences of the word within the words of the text. 
- * It then displays the total number of words searched and the count 
- * of how many times the word was found within those words. */
-
 #include <iostream>
 #include <fstream>
-using namespace std; 
+#include <string>
+#include <cctype> // For ispunct, tolower
+#include <sstream> // For stringstream to handle splitting
+using namespace std;
 
-//function protoype here
+
 int wordOccurrence(ifstream &file, const string &searchingWord, int &totalWords);
 
+//this function will clean each word by removing any punctuation and converting it to lowercase
+string cleanWord(const string &word) {
+
+    //creating a string variable for the cleaned version of the word
+    string cleanedWord;
+
+    //storing the word length in an int variable
+    int len = word.length();
+    
+    //loopig through each character in the word
+    for (int i = 0; i < len; ++i) {
+
+        char ch = word[i];
+        
+        //this if statement will keep the character if its an alphabetic character/number
+        //it will also keep it if its an internal hyphen/apostrphe
+        if (isalnum(ch) || (ch == '-' && i > 0 && i < len - 1) || (ch == '\'' && i > 0 && i < len - 1)) {
+            
+            //if so,we convert it to lowercase and add it to the cleaned up version of the word
+            cleanedWord += tolower(ch);
+        }
+    }
+
+    //this while loop will remove any leading punctuation  
+    while (!cleanedWord.empty() && ispunct(cleanedWord.front())) {
+        cleanedWord.erase(cleanedWord.begin());
+    }
+    
+    //this while loop will remove the trailing punctuation
+    while (!cleanedWord.empty() && ispunct(cleanedWord.back())) {
+        cleanedWord.pop_back();
+    }
+
+    //after all cleaning operations, we can return the cleaned word now
+    return cleanedWord;
+}
+
+//this function will count the number of occurences of the searching word in the file
+int wordOccurrence(ifstream &file, const string &searchingWord, int &totalWords) {
+
+    //string variable for the searching word
+    string word;
+
+    //initializing the occurences to be 0 
+    int occurrences = 0;
+
+    //cleaning and normalizing the search word
+    string cleanedSearchWord = cleanWord(searchingWord);
+
+    //while there are words to read from the input file
+    while (file >> word) {
+
+        //we will clean the word first
+        string cleanedWord = cleanWord(word);
+
+        //if the cleaned word contains a hyphen, we will split it and check both parts
+        int hyphenPos = cleanedWord.find('-');
+
+        //if the 
+        if (hyphenPos != string::npos) {
+            
+            //since the hyphen is found, the word will be split into two parts
+            
+            //this is the part of the word before the hyphen
+            string firstPart = cleanedWord.substr(0, hyphenPos);
+
+            //this is part of the word after the hyphen
+            string secondPart = cleanedWord.substr(hyphenPos + 1);
+
+            //we'll count both parts as individual words (which will lead to the total word count being larger than it should be)
+            totalWords += 2;
+
+            //if either of the first part or second part of the word, matches the searched word then...
+            if (firstPart == cleanedSearchWord || secondPart == cleanedSearchWord) {
+
+                //the number of occurences will be incremented
+                occurrences++;
+            }
+        } else {
+
+            //if no hyphen is found
+            totalWords++;
+
+            //if the cleaned word is the same as the cleaned search word, then...
+            if (cleanedWord == cleanedSearchWord) {
+
+                //the number of occurences is incremented
+                occurrences++;
+            }
+        }
+    }
+
+    //once all searching is done, the number of occurences is returned
+    return occurrences;
+}
+
 int main() {
-    string word{};
-    cout << endl;
-    cout << "Hi there! this outputs the number of times" << endl
-         << "a word appears in Hamlet by William Shakespeare" << endl;
-    cout << endl;
+
+    string word;
+    string filePath = "hamlet.txt"; // Hardcoded file name
+
+    cout << "Hi there! This outputs the number of times" << endl
+         << "a word appears in Hamlet by William Shakespeare." << endl;
     cout << "Please enter the word to search for:" << endl;
     cin >> word;
 
-    //opening the file
-    ifstream inFile("hamlet.txt");
+    // Opening the file hamlet.txt
+    ifstream inFile(filePath);
 
-    //checks if the file has been successfully opened
+    // Checking if the file has been properly opened 
     if (!inFile) {
-        cerr << "The file cannot be opened properly." << endl;
+        cerr << "The file hamlet.txt cannot be opened." << endl;
         return 1;
     }
 
     int totalWords = 0;
 
-    //calling the function that finds the word provided by the user
+    // Finding the number of occurrences by calling the function on the word
     int occurrences = wordOccurrence(inFile, word, totalWords);
 
-    //closing the file
+    // Closing the file
     inFile.close();
 
-    //outputting the final message just like how it is displayed in the assignment
+    // Outputting the final message
     cout << totalWords << " words were searched and the word \"" << word << "\" was found " << occurrences << " times." << endl;
 
     return 0;
 }
 
-//function to convert a character to lowercase
-char toLowerCase(char ch) {
-    if (ch >= 'A' && ch <= 'Z') {
-
-        //converts uppercase letters to lowercase
-        return ch + 32;
-    }
-    return ch;
-}
-
-//function to remove punctuation from a word
-string removePunctuation(const string &word) {
-
-    //passing in the string as input and created a new empty string
-    string newWord;
-
-    //for loop that loops through each character in the string
-    for (char ch : word) {
-
-        //if the character is not a punctionation mark, add it to the string
-        if (!ispunct(ch)) {
-            newWord += ch;
-        }
-    }
-
-    //we will reutrn a clean up word minus all punctuation
-    return newWord;
-}
-
-//function that converts a string to all lowercase
-string toLowerCaseWord(const string &str) {
-
-    //passing in the string
-    string lowerStr;
-    
-    //for each charatcter in the string, covert each character to lowercase using the lowercase function
-    for (char ch : str) {
-        lowerStr += toLowerCase(ch);
-    }
-
-    return lowerStr;
-}
-
-//this function will count the # of occureneces of a word in the file
-int wordOccurrence(ifstream &file, const string &searchingWord, int &totalWords) {
-    string word;
-    int occurrences = 0;
-
-    //converting the search word to lowercase for case-sensitive comparisons
-    string lowerCaseSearchWord = toLowerCaseWord(searchingWord);
-
-    //while loop while there are strings left in the input file to read
-    while (file >> word) {
-        
-        //converting the word currently being looked at to lower case
-        string lowerCaseWord = toLowerCaseWord(word);
-
-        //removing the punctuation from the word to accurately match
-        lowerCaseWord = removePunctuation(lowerCaseWord);
-
-        totalWords++;
-
-        //checking if the current word matches the search word
-        if (lowerCaseWord == lowerCaseSearchWord) {
-
-            //if it matches, increment the # of occurrences 
-            occurrences++;
-        }
-    }
-
-    //returning the # of occurences now that the entire file has been parsed through
-    return occurrences;
-}
